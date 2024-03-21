6e1c835265af94373dc73c43c054c398278384a0
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-03-22 00:46:44 +0900

    カードリスト表示実装

--- src/views/Prototype.vue
+++ src/views/Prototype.vue
@@ -1,51 +1,58 @@
 <template>
-	<v-form v-model="valid">
+	<v-main>
 		<v-container>
+			<v-form>
+					<v-row>
+					<v-col
+						cols="12"
+						md="4"
+					>
+						<v-text-field
+						v-model="newName"
+						:counter="20"
+						:rules="nameRules"
+						label="名前の追加"
+						hide-details
+						required
+						></v-text-field>
+					</v-col>
+					</v-row>
+			</v-form>
 			<v-row>
-			<v-col
+				<v-col
 				cols="12"
 				md="4"
-			>
-				<v-text-field
-				v-model="message"
-				:counter="20"
-				:rules="nameRules"
-				label="名前の追加"
-				hide-details
-				required
-				></v-text-field>
-			</v-col>
+				>
+					<v-btn
+					color="primary"
+					class="ma-2"
+					type="submit"
+					:disabled="invalid"
+					@click="addCard"
+					>
+						追加
+					</v-btn>
+				</v-col>
 			</v-row>
 		</v-container>
-	</v-form>
-	<v-btn
-		color="primary"
-		class="ma-2"
-		type="submit"
-		:disabled="invalid"
-		@click="submit"
-	>
-		追加
-	</v-btn>
-
-	<div class="d-flex align-center flex-column">
-		<v-card width="400">
-		<template v-slot:title>
-			{{ message }}
-		</template>
-	
-		<template v-slot:subtitle>
-			{{ message }}
-		</template>
-	
-		</v-card>
-		
-		<v-card width="400">
-			<v-card-item>
-				<v-card-title>{{ message }}</v-card-title>	
-			</v-card-item>
-		</v-card>
-	</div>
+		<v-container>
+			<v-card 
+			v-if="cardList && cardList.length > 0"
+			width="400">
+				<v-row>
+					<v-col
+					v-for="(card, index) in cardList"
+					:key="`${card}_${index}`"
+					cols="4"
+					>
+					<v-card-text>
+						{{ card.name }}
+					</v-card-text>
+					</v-col>
+				</v-row>
+			</v-card>
+		</v-container>
+	</v-main>
 </template>
 
 <script lang="ts">
@@ -53,7 +60,17 @@ import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
 	data: () => ({
-		message: ""
-	})
+		newName: "",
+		cardList: [] as { name: string } []
+	}),
+	methods: {
+		addCard() {
+			this.cardList.push({
+				name: this.newName
+			});
+			this.newName = '';
+			console.log("cardList", this.cardList);
+		}
+	}
 })
 </script>
