37ce04f73636591be76fadc32bbc56bc60fbb20f
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-03-24 23:05:03 +0900

    D&D実装

--- src/views/Prototype.vue
+++ src/views/Prototype.vue
@@ -35,22 +35,33 @@
 				</v-col>
 			</v-row>
 		</v-container>
-		<v-container>
-			<v-card 
+		<v-container
 			v-if="cardList && cardList.length > 0"
-			width="400">
-				<v-row>
-					<v-col
+			width="400"
+		>
+			<div
+				class="drop-area"
+				@drop="dropList($event)"
+				@dragover.prevent
+				@dragenter.prevent
+			>
+			<v-row>
+				<v-col>
+					<v-card 
 					v-for="(card, index) in cardList"
-					:key="`${card}_${index}`"
-					cols="4"
+							:key="`${card}_${index}`"
+							cols="4"
+							draggable="true"
+							@dragstart="dragList($event, card.id)"
 					>
 					<v-card-text>
+						{{ card.id }}
 						{{ card.name }}
 					</v-card-text>
-					</v-col>
-				</v-row>
-			</v-card>
+					</v-card>
+				</v-col>
+			</v-row>
+			</div>
 		</v-container>
 	</v-main>
 </template>
@@ -61,15 +72,32 @@ import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
 	data: () => ({
 		newName: "",
-		cardList: [] as { name: string } []
+		cardList: [] as { id: number , name: string } [],
+		nextId: 1
 	}),
 	methods: {
 		addCard() {
 			this.cardList.push({
+				id: this.nextId++,
 				name: this.newName
 			});
 			this.newName = '';
-			console.log("cardList", this.cardList);
+		},
+		dragList(event, dragId) {
+			event.dataTransfer.effectAllowed = 'move'
+			event.dataTransfer.dropEffect = 'move'
+			event.dataTransfer.setData('CardListId', dragId)
+		},
+		dropList(event) {
+			const dragId = parseInt(event.dataTransfer.getData('CardListId'))
+			// const dragList = this.cardList.find(card => card.id === dragId)
+			const dragListIndex = this.cardList.findIndex(card => card.id === dragId);
+			if (dragListIndex !== -1) {
+				// ドロップされた要素のインデックスを取得する
+				const dropIndex = dragListIndex - 1;
+				// 要素をドロップされた位置に挿入する
+				this.cardList.splice(dropIndex, 0, this.cardList.splice(dragListIndex, 1)[0]);
+			}
 		}
 	}
 })
